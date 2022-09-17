a521eb2bfc70b17e329132b8f32a689100d77a11
dusangojkovic87 <dusangojkovic087@gmail.com>
2022-09-17 20:37:09 +0200

    dodat store u auth guard

--- src/app/Shared/guards/auth.guard.ts
+++ src/app/Shared/guards/auth.guard.ts
@@ -2,21 +2,35 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PersistenceService } from 'src/app/Services/persistence.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/appReducer/appReducer';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  isAllowed: boolean = false;
   constructor(
     private persistanceService: PersistenceService,
-    private jwtHelper: JwtHelperService
-  ) {}
+    private jwtHelper: JwtHelperService,
+    private store: Store<AppState>,
+    private router: Router
+  ) {
+    this.store
+      .select((state) => state.authState.isAuthenticated)
+      .subscribe((data) => {
+        if (data) {
+          this.isAllowed = data;
+        }
+      });
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -26,7 +40,11 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    let token = this.persistanceService.get('token');
-    return !this.jwtHelper.isTokenExpired(token);
+    if (this.isAllowed == false) {
+      this.router.navigateByUrl('/auth/login');
+      return false;
+    }
+
+    return this.isAllowed;
   }
 }
--- src/app/appReducer/appReducer.ts
+++ src/app/appReducer/appReducer.ts
@@ -1,9 +1,11 @@
 import { authReducer } from '../pages/auth/authReducer/auth.reducer';
+import { AuthStateInterface } from '../pages/auth/types/AuthStateInterface';
 import { headerReducer } from '../Shared/sharedReducer/headerReducer';
 import { HeaderInterfaceState } from '../Shared/types/headerInterfaceState';
 
 export interface AppState {
   headerState: HeaderInterfaceState;
+  authState: AuthStateInterface;
 }
 
 export const appReducer = {
