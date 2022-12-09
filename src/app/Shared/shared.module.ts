0718ff3b21510549c1a900abcadc77e8d7024810
dusangojkovic87 <dusangojkovic087@gmail.com>
2022-12-09 16:01:14 +0100

    forma za edit profile dodata

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -2,10 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
+import { EditProfileComponent } from '../pages/editProfile/components/edit-profile/edit-profile.component';
 
 import { DashboardNavComponent } from './components/dashboard-nav/dashboard-nav.component';
+const routes: Routes = [
+  { path: 'edit-profile', component: EditProfileComponent },
+];
 
 @NgModule({
   declarations: [DashboardNavComponent],
@@ -13,7 +17,7 @@ import { DashboardNavComponent } from './components/dashboard-nav/dashboard-nav.
     BrowserModule,
     CommonModule,
     HttpClientModule,
-    RouterModule,
+    RouterModule.forChild(routes),
     EffectsModule.forFeature([]),
   ],
   providers: [],
