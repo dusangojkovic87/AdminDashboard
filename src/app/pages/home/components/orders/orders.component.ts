1c95878977e8ccb099e9563f6785db5f27abf99e
dusangojkovic87 <dusangojkovic087@gmail.com>
2023-02-07 09:12:45 +0100

    dodate rxjs unsubscribe za store i notification u orders komponentu

--- src/app/pages/home/components/orders/orders.component.ts
+++ src/app/pages/home/components/orders/orders.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { NotifierService } from 'angular-notifier';
+import { Subscription } from 'rxjs';
 import { AppState } from 'src/app/appReducer/appReducer';
 import { CustomerOrder } from '../customer-order-list/types/CustomerOrder';
 import {
@@ -13,9 +14,11 @@ import {
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss'],
 })
-export class OrdersComponent implements OnInit {
+export class OrdersComponent implements OnInit, OnDestroy {
   orders: CustomerOrder[] = [];
   p: number = 1;
+  ordersSub!: Subscription;
+  notificationSub!: Subscription;
 
   constructor(
     private store: Store<AppState>,
@@ -33,7 +36,7 @@ export class OrdersComponent implements OnInit {
   }
 
   getOrdersFromStore() {
-    this.store
+    this.ordersSub = this.store
       .select((state) => state.ordersState.filteredOrders)
       .subscribe((data) => {
         if (data) {
@@ -43,15 +46,20 @@ export class OrdersComponent implements OnInit {
   }
 
   isOrderStatusChangedNotification() {
-    this.store
+    this.notificationSub = this.store
       .select((state) => state.ordersState.isOrderStatusChanged)
       .subscribe((isStatusChanged) => {
-        console.log(isStatusChanged);
-
         if (isStatusChanged) {
           this.notifier.notify('success', 'order status changed');
           this.store.dispatch(setOrderStatusToDefault());
         }
       });
+  }
+
+  ngOnDestroy(): void {
+    if (this.notificationSub && this.ordersSub) {
+      this.ordersSub.unsubscribe();
+      this.notificationSub.unsubscribe();
+    }
   }
 }
