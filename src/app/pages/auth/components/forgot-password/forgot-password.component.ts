7020d8fee06833e99941810f2cfb90083b4482f1
dusangojkovic87 <dusangojkovic087@gmail.com>
2023-06-23 22:36:30 +0200

    responsive forgot pass dodat

--- src/app/pages/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/pages/auth/components/forgot-password/forgot-password.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  emailForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      email: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  recoverPassword() {
+    //add pass recovery logic here
+    this.emailForm.reset();
+  }
 }
