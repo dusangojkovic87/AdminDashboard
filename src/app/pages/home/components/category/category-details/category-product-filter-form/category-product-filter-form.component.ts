9e824e961cad3ccbe223a3fd5b298343ba9a33b4
dusangojkovic87 <dusangojkovic087@gmail.com>
2023-06-28 10:19:20 +0200

    responsive product table in category details dodato

--- src/app/pages/home/components/category/category-details/category-product-filter-form/category-product-filter-form.component.ts
+++ src/app/pages/home/components/category/category-details/category-product-filter-form/category-product-filter-form.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/appReducer/appReducer';
+import {
+  closeProductsModal,
+  openProductsModal,
+} from '../../../products/productActions/productActions';
 
 @Component({
   selector: 'app-category-product-filter-form',
   templateUrl: './category-product-filter-form.component.html',
-  styleUrls: ['./category-product-filter-form.component.scss']
+  styleUrls: ['./category-product-filter-form.component.scss'],
 })
-export class CategoryProductFilterFormComponent implements OnInit {
+export class CategoryProductFilterFormComponent implements OnInit, OnDestroy {
+  constructor(private store: Store<AppState>) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openAddProductModal() {
+    this.store.dispatch(openProductsModal());
   }
 
+  ngOnDestroy(): void {
+    this.store.dispatch(closeProductsModal());
+  }
 }
